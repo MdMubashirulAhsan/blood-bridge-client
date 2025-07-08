cb1e21b3f6020406f226a197c637c8c96eb1de86
MdMubashirulAhsan <ahsan.mubashirul@gmail.com>
2025-07-08 21:02:31 +0600

    added hooks like useAxiosSecure, useUserRole

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router';
 const axiosSecure = axios.create({
     baseURL: `https://zap-shift-server-psi.vercel.app`
 });
-
+// ksjdfsf
 const useAxiosSecure = () => {
     const { user, logOut } = useAuth();
     const navigate = useNavigate();
